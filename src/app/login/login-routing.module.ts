0a6a80c6c30586479e80a7503d1e25a196fc5b01
ricardosantanadev <ricardosantanadev@gmail.com>
2024-04-21 09:48:55 -0300

    update

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './containers/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from '../guards/auth-guard'
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
--- src/app/login/services/auth.service.ts
+++ src/app/login/services/auth.service.ts
@@ -6,16 +6,26 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class AuthService {
+  // mostrarMenuEmitter = new EventEmitter<Boolean>();
+  private usuarioAutenticado = false;
 
   constructor(private router: Router) { }
 
   login(form: FormGroup) {
     if (form.value.useremail == 'user@email.com' && form.value.password == '123456') {
+      this.usuarioAutenticado = true;
+      // this.mostrarMenuEmitter.emit(true);
       // this.router.navigate(['dashboard'], {relativeTo: this.route}); nao funciona porque a service nao tem rota
       this.router.navigate(['/login/dashboard'])
       console.log('Usuário autenticado!');
     } else {
+      this.usuarioAutenticado = false;
+      // this.mostrarMenuEmitter.emit(false);
       console.log('Usuário não autenticado!');
     }
+  }
+
+  usuarioEstaAutenticado() {
+    return this.usuarioAutenticado;
   }
 }
